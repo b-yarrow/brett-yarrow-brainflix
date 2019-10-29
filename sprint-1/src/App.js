a82e71c3cbca58639862271a63ec14ad9735a027
Brett Yarrow <byarrow86@gmail.com>
2019-10-29 14:47:47 -0700

    Mobile comments section first pass.

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -2,13 +2,48 @@ import React from 'react';
 import Header from './components/Header';
 import Video from './components/Video';
 import VideoInfo from './components/VideoInfo';
+import Comments from './components/Comments';
+
+function commentObj(name, avatar, date, comment) {
+  this.name = name;
+  this.avatar = avatar;
+  this.date = date;
+  this.comment = comment;
+}
+
+function videoObj(title, creator, date, preview) {
+  this.title = title;
+  this.creator = creator;
+  this.date = date;
+  this.preview = preview;
+}
+
+
 
 function App() {
+  const commentAry = [
+    new commentObj('Theodore Duncan', 'blank.jpg', '11/15/2018', 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever!'),
+    new commentObj('Gary Wong', 'blank.jpg', '12/12/2018', 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'),
+    new commentObj('Michael Lyons', 'blank.jpg', '12/18/2018', 'They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.')
+  ];
+
+  const videoList = [
+    new videoObj('BMX Rampage: 2018 Highlights', 'Red Cow', '12/18/2018', 'video-list-0.jpg'),
+    new videoObj('Become A Travel Pro In One Easy Lesson…', 'Scotty Cranmer', '12/18/2018', 'video-list-1.jpg'),
+    new videoObj('Les Houches The Hidden Gem Of The…', 'Scotty Cranmer', '12/18/2018', 'video-list-2.jpg'),
+    new videoObj('Travel Health Useful Medical Information…', 'Scotty Cranmer', '12/18/2018', 'video-list-3.jpg'),
+    new videoObj('Cheap Airline Tickets Great Ways To Save', 'Emily Harper', '12/18/2018', 'video-list-4.jpg'),
+    new videoObj('Take A Romantic Break In A Boutique Hotel', 'Ethan Owen', '12/18/2018', 'video-list-5.jpg'),
+    new videoObj('Choose The Perfect Accommodations', 'Lydia Perez', '12/18/2018', 'video-list-6.jpg'),
+    new videoObj('Cruising Destination Ideas', 'Timothy Austin', '12/18/2018', 'video-list-7.jpg'),
+    new videoObj('Train Travel On Track For Safety', 'Scotty Cranmer', '12/18/2018', 'video-list-8.jpg')
+  ]
   return (
     <div>
       <Header />
       <Video />
       <VideoInfo />
+      <Comments commentAry={commentAry} />
     </div>
   );
 }
---
+++ sprint-1/src/components/Comments.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import picBlank from '../assets/images/blank.jpg'
+import picUser from '../assets/images/Mohan-muruge.jpg'
+
+const picPath = '../assets/images/'
+
+export default function Comments(props) {
+    const comments = props.commentAry.map((comment, index) => {
+        console.log(comment);
+        console.log(comment.name);
+        return (
+            <div key={index} className="comments__post">
+                <div className="comments__image-box">
+                    <img className="comments__image" src={picBlank}></img>
+                </div>
+                <div className="comments__text-box">
+                    <header className="comments__header">
+                        <h2 className="comments__name">{comment.name}</h2>
+                        <h5 className="comments__date">{comment.date}</h5>
+                    </header>
+                    <p className="comments__comment">
+                        {comment.comment}
+                    </p>
+                    {/* <footer className="comments__footer">
+                        <div className="comments__like-container">
+                            <button className="comments__like">like</button>
+                            &nbsp;&#8226;&nbsp;<span className="comments__like-value">###</span>
+                        </div>
+                        <button className="comments__delete">delete</button>
+                    </footer> */}
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <section className="comments">
+            <h1 className="comments__title">{3} Comments</h1>
+            <div className="comments__container">
+                <div className="comments__post comments__post--create">
+
+                    <div className="comments__image-box comments__image-box--create">
+                        <img name="commentImage" className="comments__image comments__image--create"
+                            src={picUser}></img>
+                    </div>
+                    <form className="comments__name--create" id="commentForm">
+                        <div className="comments__text-box comments__text-box--create">
+                            <div className="comments__comment comments__comment--create">
+                                <label htmlFor="fcomment">JOIN THE CONVERSATION</label>
+                                <textarea className="comments__comment-input" name="commentText" id="fcomment" cols="5" rows="5" placeholder="Add a new comment"></textarea>
+                                <button className="comments__button" name="commentButton">
+                                    COMMENT
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div className="comments__post-container">
+                    {comments}
+                </div>
+            </div>
+        </section>
+    )
+}
