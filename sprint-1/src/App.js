86ca9488ec130d97b888f4079a5aefdcb4d292e0
Brett Yarrow <byarrow86@gmail.com>
2019-10-28 17:26:41 -0700

    Mobile Header first pass.  Mobile video player started.

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -1,49 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import logoBF from './Logo-brainflix.svg';
-import profPic from './blank.jpg'
-import './App.css';
+import Header from './components/Header';
+import Video from './components/Video';
 
 function App() {
   return (
-
-    <div className="App">
-      <header class="header">
-        <a href="index.html">
-          <img class="header__logo" src={logoBF} alt="Brainflix Logo" />
-        </a>
-        <nav class="header__list">
-          <div class="header__list-item header__list-item--active">
-            <i class="material-icons">search</i>
-            <input class="header__name-input" type="search" name="profileName" id="fname"
-              placeholder="Enter Your Name"></input>
-          </div>
-          <div class="header__list-item">
-            <button class="header__button" name="uploadtButton">
-              + UPLOAD
-              </button>
-            <div class="header__image-box conversation__image-box--create">
-              <img name="commentImage" class="header__image header__image--create"
-                src={profPic} />
-            </div>
-
-          </div>
-        </nav>
-      </header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Video />
     </div>
   );
 }
---
+++ sprint-1/src/components/Video.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import vidPoster from '../assets/images/video-list-0.jpg'
+import vidFile from '../assets/video/brainStationSampleVideo.mp4';
+import playButton from '../assets/images/icons/SVG/icon-play.svg'
+import fullscreen from '../assets/images/icons/SVG/Icon-fullscreen.svg'
+import volume from '../assets/images/icons/SVG/Icon-volume.svg'
+
+export default function Video() {
+    return (
+        <div className="video__container">
+            <div className="video__player-box">
+                <video width="320" height="183" poster={vidPoster}>
+                    <source src={vidFile} type="video/mp4"></source>
+                </video>
+                <div className="video__buttons">
+                    <div className="video__play-button">
+                        <img src={playButton} />
+                    </div>
+                    <div className="video__scrubber">
+                        <progress className="video__progBar" value={0} max={100}></progress>
+                    </div>
+                    <div className="video__fs-vol">
+                        <img src={fullscreen} />
+                        <img src={volume} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
